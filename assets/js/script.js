2c0744a8e87430b3b432f176233f2fcdecdde417
stephen Batelaan <sbatelaan91@gmail.com>
2023-02-13 20:29:20 -0800

    added weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,25 @@
 var rapidapikey = "c847c94837msh769cdaeccd7bcbdp1df16ajsn03d64a2f2860"
 var weatherApi = "a7cf6d8b7818bef7a79d89c363469555"
 var weatherIcon = document.querySelector("img");
-var hideImg = document.getElementsByClassName("hide");
-var resultEl = docment.querySelector('#result');
+var resultEl = document.querySelector('#result');
 var resultCo = document.querySelector('#result-contents');
-var searchFormEl = document.querySelector('#search=form');
+var searchFormEl = document.querySelector('#search-form');
+var tempText = document.getElementById("temp")
+var weatherName = document.getElementById("city-name")
+var weatherDescription = document.getElementById("weather")
+
+
+function addHidden() {
+
+      $("#rainy").removeClass()
+      $("#cloudy").removeClass()
+      $("#clear").removeClass()
+      $("#snowy").removeClass()
+    searchCity()
+  }
+
+
+
 
 function searchCity(cityName) {
   var cityName = document.getElementById("city-input").value
@@ -15,20 +30,32 @@ function searchCity(cityName) {
         })
         .then(function (data) {
           console.log(data)
-          var description = data.weather[0].description
+          var mainWeather = data.weather[0].main
           var iconCode = data.weather[0].icon
           var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
           $('#wicon').attr("src", iconUrl);
-          console.log(description)
-          if (description === 'rain'){
-           $("#hide").attr("id", "bkrnd-img")
-          }
+          console.log(mainWeather)
+          weatherName.innerText = data.name
+          tempText.innerText = "Temp: " + data.main.temp + "°F"
+          weatherDescription.innerText = data.weather[0].description
+         
+           if (mainWeather === 'Rain'){
+            $("#rainy").attr("class", "bkrnd-img")
+           }
+           else if (mainWeather === 'Clear'){
+            $("#clear").attr("class", "bkrnd-img")
+           }
+           else if (mainWeather === 'Clouds'){
+            $("#cloudy").attr("class", "bkrnd-img")
+           }
+           else if (mainWeather === 'Snow'){
+            $("#snowy").attr("class", "bkrnd-img")
+           }
           
-        })
-}
+        })}
 
 
 
 
 
-document.getElementById("btn").addEventListener("click", searchCity)
+document.getElementById("btn").addEventListener("click", addHidden)
