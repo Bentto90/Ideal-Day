b7528f59da3bcb32c16feb4ec51ba32baa4f537d
Kaden Inskeep <tsukadenskeep@gmail.com>
2023-02-13 22:10:58 -0800

    added day planner

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,9 @@ var rapidapikey = "c847c94837msh769cdaeccd7bcbdp1df16ajsn03d64a2f2860"
 var weatherApi = "a7cf6d8b7818bef7a79d89c363469555"
 var weatherIcon = document.querySelector("img");
 var hideImg = document.getElementsByClassName("hide");
-var resultEl = docment.querySelector('#result');
+var resultEl = document.querySelector('#result');
 var resultCo = document.querySelector('#result-contents');
-var searchFormEl = document.querySelector('#search=form');
+var searchFormEl = document.querySelector('#search-form');
 
 function searchCity(cityName) {
   var cityName = document.getElementById("city-input").value
@@ -28,7 +28,29 @@ function searchCity(cityName) {
 }
 
 
-
-
-
 document.getElementById("btn").addEventListener("click", searchCity)
+
+
+// jquery js
+$( function() {
+  $( ".column" ).sortable({
+    connectWith: ".column",
+    handle: ".portlet-header",
+    cancel: ".portlet-toggle",
+    placeholder: "portlet-placeholder ui-corner-all"
+  });
+
+  $( ".portlet" )
+    .addClass( "ui-widget ui-widget-content ui-helper-clearfix ui-corner-all" )
+    .find( ".portlet-header" )
+      .addClass( "ui-widget-header ui-corner-all" )
+      .prepend( "<span class='ui-icon ui-icon-minusthick portlet-toggle'></span>");
+
+  $( ".portlet-toggle" ).on( "click", function() {
+    var icon = $( this );
+    icon.toggleClass( "ui-icon-minusthick ui-icon-plusthick" );
+    icon.closest( ".portlet" ).find( ".portlet-content" ).toggle();
+  });
+} );
+// end
+
